22f2779406c10f5334fbe89f0fe717385e759ac7
Melissa Wong <mellymai@gmail.com>
2018-04-13 12:37:13 -0700

    Removes explicit routing to simplify URL string

--- application/config/routes.php
+++ application/config/routes.php
@@ -55,9 +55,9 @@ $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 
 
-$route['default_controller'] = 'welcome';
+//$route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-//$route['default_controller'] = 'pages/view';
-//$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
+$route['(:any)'] = 'pages/view/$1';
