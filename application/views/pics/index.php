a8667e2e82dca18c5f2c998c274e0fb6c203ac23
Melissa Wong <mellymai@gmail.com>
2018-05-01 17:08:05 -0700

    Adds site_url to picture links

--- application/views/pics/index.php
+++ application/views/pics/index.php
@@ -4,9 +4,9 @@
 $this->load->view($this->config->item('theme') . 'header'); ?>
 <h2>Search Examples</h2>
 <ul>
-  <li><a href="/pics/bowling">bowling</a></li>
-  <li><a href="/pics/lacrosse">lacrosse</a></li>
-  <li><a href="/pics/backgammon">backgammon</a></li>
+  <li><a href="<?=site_url()?>pics/bowling">bowling</a></li>
+  <li><a href="<?=site_url()?>pics/lacrosse">lacrosse</a></li>
+  <li><a href="<?=site_url()?>pics/backgammon">backgammon</a></li>
 </ul>
 
 <?php $this->load->view($this->config->item('theme') . 'footer'); ?>
